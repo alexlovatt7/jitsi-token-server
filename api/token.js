f7f8d22926ff8448cf2607e81386d3a53194f225
Alex Lovatt <alexlovatt@MacBookPro.attlocal.net>
2025-06-29 11:38:35 -0400

    Update index.js to support Vercel environment variables and root route

--- api/token.js
+++ api/token.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const APP_ID = process.env.APP_ID || 'vpaas-magic-cookie-c676d66f911c49e582272680109cda13';
-const SUB = process.env.SUB || '8x8.vc';
+// Remove SUB since we'll use APP_ID for both iss and sub
 const PRIVATE_KEY = process.env.PRIVATE_KEY.replace(/\\n/g, '\n');
 
 export default function handler(req, res) {
@@ -24,8 +24,8 @@ export default function handler(req, res) {
 
   const payload = {
     aud: 'jitsi',
-    iss: APP_ID,
-    sub: SUB,
+    iss: APP_ID,  // This should be your full vpaas APP_ID
+    sub: APP_ID,  // For vpaas, sub should also be the APP_ID, not '8x8.vc'
     room,
     exp: Math.floor(Date.now() / 1000) + 60 * 60,  // expires in 1 hour
     nbf: Math.floor(Date.now() / 1000),
